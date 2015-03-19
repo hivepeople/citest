2c375ec4893a5cd0b7cee1f9ae18ffe50220ed0c
sboisen <snb@hivepeople.dk>
2015-03-19 10:42:56 +0100

    add: git ignore file add: basic console project to test build

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HivePeople.CITest
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.Out.WriteLine("-= CI integration test =-");
+            Console.Out.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
